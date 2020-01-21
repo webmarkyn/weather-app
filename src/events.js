4a28569da80c3d72e098cbd63bf12f3e3c8d39a4
webmarkyn <web.markyn@gmail.com>
2020-01-21 12:38:58 +0300

    fixes

--- src/events.js
+++ src/events.js
@@ -1,18 +1,17 @@
-function openPopup(popup) {
+const openPopup = (popup) => {
   popup.style.visibility = 'visible';
   popup.style.opacity = '1';
-}
+};
 
-function closePopup(popup) {
-  return new Promise((resolve) => {
-    popup.style.opacity = '0';
-    const trans = () => {
-      popup.style.visibility = 'hidden';
-      popup.removeEventListener('transitionend', trans);
-      resolve();
-    };
-    popup.addEventListener('transitionend', trans);
-  });
-}
+const closePopup = (popup) => new Promise((resolve) => {
+  popup.style.opacity = '0';
+  const trans = () => {
+    popup.style.visibility = 'hidden';
+    popup.removeEventListener('transitionend', trans);
+    resolve();
+  };
+  popup.addEventListener('transitionend', trans);
+});
+
 
 export { openPopup, closePopup };
--- src/weather.js
+++ src/weather.js
@@ -4,12 +4,12 @@ import {
 } from './dom';
 import { openPopup, closePopup } from './events';
 
-async function getWeather(city, units) {
+const getWeather = (city, units) => {
   const url = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=1a7d76daf845e6d9571a1071f31062f1`;
   return fetch(url).then(data => data.json());
-}
+};
 
-function renderWeather(data) {
+const renderWeather = (data) => {
   weatherInfo.innerHTML = `
       <h2 class="weather-city">${data.name}</h2>
       <div class="weather-color" style="background: ${
@@ -20,9 +20,9 @@ function renderWeather(data) {
       <div class="weather-desc">${data.weather[0].main}</div>
       <div class="weather-temp">${data.main.temp}°</div>
     `;
-}
+};
 
-export default function loadWeather(city, units) {
+const loadWeather = (city, units) => {
   openPopup(loader);
   getWeather(city, units).then((data) => {
     openPopup(overlay);
@@ -33,4 +33,7 @@ export default function loadWeather(city, units) {
     closePopup(overlay);
     renderWeather(data);
   });
-}
+};
+
+
+export default loadWeather;
