6aca6c457f32fb767d32d16abc899072e088fa4e
webmarkyn <web.markyn@gmail.com>
2020-01-21 12:41:45 +0300

    removed parentheses in events.js

--- src/events.js
+++ src/events.js
@@ -3,7 +3,7 @@ const openPopup = (popup) => {
   popup.style.opacity = '1';
 };
 
-const closePopup = (popup) => new Promise((resolve) => {
+const closePopup = popup => new Promise((resolve) => {
   popup.style.opacity = '0';
   const trans = () => {
     popup.style.visibility = 'hidden';
